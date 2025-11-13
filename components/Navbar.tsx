fdffd39cdb3ea83ccb834a3f974da671d35c627e
Kush Vardhan <kushvardhan39797@gmail.com>
2025-11-13 18:05:04 +0530

    making nav texts responsive

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -63,7 +63,7 @@ export default function Navbar() {
               <Link
                 key={link.href}
                 href={link.href}
-                className="px-4 py-2 text-xl font-medium text-gray-300 hover:text-white transition-colors relative group"
+                className="px-4 py-2 text-regular font-medium text-gray-300 hover:text-white transition-colors relative group"
               >
                 {link.label}
                 <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-linear-to-r from-(--accent-gold) to-(--accent-cyan) group-hover:w-full transition-all duration-300" />
